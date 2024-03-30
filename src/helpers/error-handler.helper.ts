e27c452a333d3508db1085e6b7054f1c6b230a29
Anh Dev <info.anhdev@gmail.com>
2024-03-30 11:28:59 +0700

    fix: bug 'cannot set headers after they are sent to the client'

---
+++ src/helpers/error-handler.helper.ts
@@ -0,0 +1,46 @@
+import { ApolloServerErrorCode } from '@apollo/server/errors'
+import { GraphQLError } from 'graphql'
+
+interface ErrorType {
+  errorCode: string | ApolloServerErrorCode
+  errorStatus: number
+}
+
+export const ErrorTypes: Record<string, ErrorType> = {
+  BAD_USER_INPUT: {
+    errorCode: ApolloServerErrorCode.BAD_USER_INPUT,
+    errorStatus: 400
+  },
+  BAD_REQUEST: {
+    errorCode: ApolloServerErrorCode.BAD_REQUEST,
+    errorStatus: 400
+  },
+  NOT_FOUND: {
+    errorCode: 'NOT_FOUND',
+    errorStatus: 404
+  },
+  UNAUTHENTICATED: {
+    errorCode: 'UNAUTHENTICATED',
+    errorStatus: 401
+  },
+  ALREADY_EXISTS: {
+    errorCode: 'ALREADY_EXISTS',
+    errorStatus: 400
+  },
+  INTERNAL_SERVER_ERROR: {
+    errorCode: ApolloServerErrorCode.INTERNAL_SERVER_ERROR,
+    errorStatus: 500
+  }
+}
+
+// throwCustomError function
+const throwCustomError = (errorMessage: string, errorType: ErrorType): never => {
+  throw new GraphQLError(errorMessage, undefined, undefined, undefined, undefined, undefined, {
+    code: errorType.errorCode,
+    http: {
+      status: errorType.errorStatus
+    }
+  })
+}
+
+export default throwCustomError
